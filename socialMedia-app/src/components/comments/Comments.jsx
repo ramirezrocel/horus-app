d35c4624792413e40b591955cfa1ece58eab6c54
Ashley8210 <124594182+Ashley8210@users.noreply.github.com>
2023-02-23 16:54:23 +0800

    posteditcommit

--- socialMedia-app/src/components/comments/Comments.jsx
+++ socialMedia-app/src/components/comments/Comments.jsx
@@ -6,19 +6,17 @@ const Comments = () => {
   const comments = [
     {
       id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "John Doe",
+      desc: "Me bro!",
+      name: "TheLorax",
       userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      profilePicture: "./pictures/lorax.jpg",
     },
     {
       id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Jane Doe",
+      desc: "Tips on how to talk to someone while they're gaming: Don't.",
+      name: "RainbowBrite",
       userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      profilePicture: "./pictures/brite.jpg",
     },
   ];
   return (

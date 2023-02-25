5e9f79d0cc1300d3939e6834cb2c003deefb7bc4
juanNuestro <jcnuestro21@gmail.com>
2023-02-25 20:29:23 +0800

    addpost4

--- socialMedia-app/src/services/post.js
+++ socialMedia-app/src/services/post.js
@@ -3,3 +3,48 @@ import http from "./http";
 export function fetchPosts() {
   return http.get("/posts");
 }
+
+export function fetchAllPosts() {
+  return http.get("/posts/all");
+}
+
+export function fetchPostById(id) {
+  return http.get(`/posts/${id}`);
+}
+
+// export function addPost(post) {
+//   const postClone = { ...post };
+//   Object.keys(postClone).forEach((key) => {
+//     if (
+//       postClone[key] === "" ||
+//       postClone[key] === null ||
+//       postClone[key] === undefined
+//     ) {
+//       delete postClone[key];
+//     }
+//   });
+
+//   return http.post("/posts", postClone);
+// }
+
+export function addPost(value, postImageURL) {
+  return http.post("/posts", { value, postImageURL });
+}
+
+export function updatePost(id, post) {
+  const postClone = { ...post };
+  Object.keys(postClone).forEach((key) => {
+    if (
+      postClone[key] === "" ||
+      postClone[key] === null ||
+      postClone[key] === undefined
+    ) {
+      delete postClone[key];
+    }
+  });
+  return http.put(`/posts/${id}`, postClone);
+}
+
+export function deletePost(id) {
+  return http.delete(`/posts/${id}`);
+}

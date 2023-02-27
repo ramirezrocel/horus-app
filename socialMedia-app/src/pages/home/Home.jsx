121d717afbc83e511c324e56ad640605eb8da05c
Rocel Ramirez <75878316+ramirezrocel@users.noreply.github.com>
2023-02-27 16:28:40 +0800

    fetch-comment

--- socialMedia-app/src/pages/home/Home.jsx
+++ socialMedia-app/src/pages/home/Home.jsx
@@ -2,16 +2,32 @@ import Stories from "../../components/stories/Stories";
 import Posts from "../../components/posts/Posts";
 import AddPost from "../../components/share/AddPost";
 import Share from "../../components/share/Share";
+import * as postService from "../../services/post";
+import * as userService from "../../services/user";
 import "./home.scss";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    postService.fetchPosts().then((response) => {
+      setPosts(response.data);
+    });
+
+    userService.fetchUsers().then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <div className="home">
       {/* this is home */}
       <Stories />
       {/* <AddPost /> */}
       <Share></Share>
-      <Posts />
+      <Posts posts={posts} users={users} />
     </div>
   );
 };

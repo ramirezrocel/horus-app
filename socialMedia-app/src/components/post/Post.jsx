6710216e13bd3420153e6ea92ef9c2587b024508
Rocel Ramirez <75878316+ramirezrocel@users.noreply.github.com>
2023-02-27 16:12:50 +0800

    import comment.scss

--- socialMedia-app/src/components/post/Post.jsx
+++ socialMedia-app/src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import "./post.scss";
+import "../comments/comments.scss";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";

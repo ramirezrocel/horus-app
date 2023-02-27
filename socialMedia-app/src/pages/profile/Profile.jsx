e195545890d5b66d7dcc086697155c2e1806562c
Rocel Ramirez <75878316+ramirezrocel@users.noreply.github.com>
2023-02-27 21:58:21 +0800

    users/me_fetch

--- socialMedia-app/src/pages/profile/Profile.jsx
+++ socialMedia-app/src/pages/profile/Profile.jsx
@@ -1,5 +1,7 @@
 import "./profile.scss";
+import "../../components/posts/posts.scss";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import { useParams } from "react-router-dom";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import PinterestIcon from "@mui/icons-material/Pinterest";
@@ -9,10 +11,28 @@ import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Posts from "../../components/posts/Posts";
+import Post from "../../components/post/Post";
 import * as authService from "../../services/auth";
+import * as postService from "../../services/post";
+import * as profileService from "../../services/profile";
+import { useState } from "react";
+import { useEffect } from "react";
+import Share from "../../components/share/Share";
 
 const Profile = () => {
+  const [posts, setPosts] = useState([]);
+  const params = useParams();
   const currentUser = authService.getCurrentUser();
+
+  useEffect(() => {
+    profileService.fetchUserPost(currentUser.username).then((response) => {
+      setPosts(response.data);
+      // console.log(response.data);
+    });
+  }, []);
+  // console.log(posts);
+  // const filteredPosts = posts.filter((posts) => posts.userId === params.id);
+  // console.log(filteredPosts);
   return (
     <div className="profile">
       <div className="images">
@@ -59,7 +79,12 @@ const Profile = () => {
             </div>
           </div>
         </div>
-        <Posts />
+        <Share></Share>
+        <div className="posts">
+          {posts.map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
+        </div>
       </div>
     </div>
   );

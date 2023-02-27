e22c7002b9f339610ce91ad14ece5c2b8921cb00
Rocel Ramirez <75878316+ramirezrocel@users.noreply.github.com>
2023-02-27 21:50:46 +0800

    users/me_fetch

--- socialMedia-api/src/post/dto/create-post.dto.ts
+++ socialMedia-api/src/post/dto/create-post.dto.ts
@@ -1,9 +1,9 @@
+//fetch-user/me.posts
 import { IsNotEmpty, Length } from "class-validator";
 
 export class CreatePostDto {
   @IsNotEmpty()
   @Length(3)
   value: string;
-
   postImageURL: string;
 }

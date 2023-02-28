fd7d8c5713115eca877c64f36c1db5db12774d20
juanNuestro <124562469+juanNuestro@users.noreply.github.com>
2023-02-28 15:23:08 +0800

    editPost

--- socialMedia-api/src/post/dto/update-post.dto.ts
+++ socialMedia-api/src/post/dto/update-post.dto.ts
@@ -2,7 +2,6 @@ import { IsNotEmpty, IsOptional, Length } from 'class-validator';
 
 export class UpdatePostDto {
   @IsOptional()
-  @Length(3)
-  title: string;
-  completed: boolean;
+  value: string;
+  postImageURL: string;
 }

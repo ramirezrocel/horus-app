d39f58245b40fe2f8701b7f590ee0085500a8331
juanNuestro <124562469+juanNuestro@users.noreply.github.com>
2023-03-01 17:17:58 +0800

    DropDown logout

---
+++ socialMedia-app/src/components/navbar/DropDownProfile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Drop.scss";
+import { Button } from "@mui/material";
+
+const DropDownProfile = (onLogout) => {
+  return (
+    <div className="flex flex-col dropDownProfile">
+      <ul className="flex flex-col gap-4">
+        <Button style={{ textDecoration: "none", color: "inherit" }}>
+          Profile
+        </Button>{" "}
+        <hr />{" "}
+        <Button style={{ textDecoration: "none", color: "inherit" }}>
+          Settings
+        </Button>
+        <hr />
+        <Button
+          href="/login"
+          onClick={onLogout}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          Logout
+        </Button>
+      </ul>
+    </div>
+  );
+};
+
+export default DropDownProfile;

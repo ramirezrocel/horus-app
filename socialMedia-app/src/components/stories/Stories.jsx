c6dc25bb8254b2693cbc820f32dc907ddb751f7a
Ashley8210 <124594182+Ashley8210@users.noreply.github.com>
2023-03-01 09:50:09 +0800

    video commit

--- socialMedia-app/src/components/stories/Stories.jsx
+++ socialMedia-app/src/components/stories/Stories.jsx
@@ -6,7 +6,13 @@ import * as authService from "../../services/auth";
 const Stories = () => {
   return (
     <a href="https://epulze.com/?utm_source=google&utm_medium=cpc&utm_campaign=15176570628&utm_content=133307518950&utm_term=gaming%20tournaments&gclid=Cj0KCQiAutyfBhCMARIsAMgcRJRWTsUCXhvVb8cAMQ07Nkf9j0oaMdGjqEcWeHdoUQ81afL3YW3ARJgaApZYEALw_wcB">
-      <img className="tournament" src="./pictures/a.jpg"></img>
+      <video
+        controls
+        src="./pictures/video.mp4"
+        poster="./pictures/a.JPG"
+        className="tournament"
+        // width="20"
+      ></video>
     </a>
   );
 };

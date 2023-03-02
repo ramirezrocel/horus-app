2f086c70c427aed836d2568b638129ad41938309
juanNuestro <jcnuestro21@gmail.com>
2023-03-02 08:57:08 +0800

    Search-Bar

--- socialMedia-app/src/components/navbar/Navbar.jsx
+++ socialMedia-app/src/components/navbar/Navbar.jsx
@@ -1,37 +1,34 @@
 import "./navbar.scss";
+import "./Drop.scss";
+import "../../style.scss";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
-import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "../../context/darkModeContext";
-// import { AuthContext } from "../../context/authContext";
 import * as authService from "../../services/auth";
+import DropDownProfile from "./DropDownProfile";
+import * as profileService from "../../services/profile";
+import SearchBar from "./SearchBar";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = ({ onLogout }) => {
   const { toggle, darkMode } = useContext(DarkModeContext);
-  // const { currentUser } = useContext(AuthContext);
   const currentUser = authService.getCurrentUser();
+  const [openProfile, setOpenProfile] = useState(false);
+
+  // console.log(search);
+
   return (
     <div className="navbar">
       <div className="left">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <img className="horuslogo" src="../pictures/horuslogo.png"></img>
-          <img className="namelogo" src="../pictures/namelogo.png"></img>
-          {/* <span>HORUS</span> */}
+          <img className="horuslogo" src="../../pictures/horuslogo.png"></img>
+          <img className="namelogo" src="../../pictures/namelogo.png"></img>
         </Link>
-
-        <div className="search">
-          <SearchOutlinedIcon />
-          <input type="text" placeholder="Search..." />
-        </div>
       </div>
+      <SearchBar />
       <div className="right">
         {darkMode ? (
           <WbSunnyOutlinedIcon onClick={toggle} />
@@ -41,18 +38,15 @@ const Navbar = ({ onLogout }) => {
         <Link to="/home" style={{ textDecoration: "none", color: "inherit" }}>
           <HomeOutlinedIcon />
         </Link>
-
-        <Button
-          href="/login"
-          onClick={onLogout}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          Logout
-        </Button>
         <div className="user">
-          {/* <img src={currentUser.profilePic} alt="" /> */}
-          {/* <span>{currentUser.name}</span> */}
+          <img
+            src={currentUser.imageUrl}
+            alt=""
+            className="text -2x1 font-semibold cursor-pointer"
+            onClick={() => setOpenProfile((prev) => !prev)}
+          ></img>
         </div>
+        {openProfile && <DropDownProfile onLogout={onLogout} />}
       </div>
     </div>
   );

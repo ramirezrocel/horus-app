166301252ead39f06f6282f8fec338858cc591f6
juanNuestro <124562469+juanNuestro@users.noreply.github.com>
2023-03-02 09:06:04 +0800

    Create SearchBar.jsx

---
+++ socialMedia-app/src/components/navbar/SearchBar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Search.css";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as profileService from "../../services/profile";
+
+const SearchBar = () => {
+  const [query, setQuery] = useState([]);
+  const [search, setSearch] = useState({ username: "" });
+  const params = useParams();
+
+  useEffect(() => {
+    profileService.fetchUsers().then((response) => {
+      setQuery(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {/* search-container */}
+      <div className="search-container">
+        {/* search-inner */}
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setSearch(e.target.value)}
+            className=""
+          />
+        </div>
+        <div className="dropdown">
+          <datalist id="adventure"></datalist>
+          {query
+            .filter((user) => {
+              if (search === "") {
+              } else if (
+                user.username
+                  .toString()
+                  .toLowerCase()
+                  .includes(search.toString().toLowerCase())
+              ) {
+                return user.username;
+              }
+            })
+            .map((user) => {
+              return (
+                <div className="dropdown-row">
+                  <Link
+                    to={`/profile/${user.username}/posts`}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    {user.username}
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;

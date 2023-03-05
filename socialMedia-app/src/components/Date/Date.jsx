ce3010600013430399406444a0f44d2d8d8693f0
Rocel Ramirez <75878316+ramirezrocel@users.noreply.github.com>
2023-03-05 23:05:01 +0800

    Delete socialMedia-app/src/components/Date directory

--- socialMedia-app/src/components/Date/Date.jsx
+++
@@ -1,8 +0,0 @@
-import { parseISO, format } from "date-fns";
-
-export default function date({ dateString }) {
-  const date = parseISO(dateString);
-  return (
-    <time dateTime={dateString}>{format(date, "LLLL d, yyyy hh:mm a")}</time>
-  );
-}
